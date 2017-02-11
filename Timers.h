38a6dcf1c14ba1c375025bdd9e51eaa2fe9829c8
HighASG936 <lz_rock@hotmail.com>
2017-02-11 00:35:44 -0600

    v1.0.0

--- Timers.h
+++ Timers.h
@@ -3,37 +3,63 @@
 
 #include "stm32f7xx_hal.h"
 
+#define MAX_NUMBER_COUNTERS 256
 //Prototipos
 void Timer_Inicializar(void);
-private void TIM7_IRQHandler(void);
 
-//Variables privadas
-TIM_HandleTypeDef Timer;
+//Estructuras y uniones
 
-//Handler de interrupcion
-void TIM7_IRQHandler(void)
+enum
 {
-	HAL_TIM_IRQHandler(&Timer);
-	//Serial_AtencionBuzzer();
-}
+	ec_Buzzer,
+	ec_Delay,
+	ec_Max
+}eCounters;
 
-/* TIM7 init function */
+typedef struct
+{
+	uint16_t MilisCounter[MAX_NUMBER_COUNTERS];
+}sCounters;
+
+
+typedef struct
+{
+	sCounters BaseTime;
+	TIM_HandleTypeDef Timer;										//Estructura del temporizador (ms) del buzzer y otros servicios
+}sTimer;
+
+sTimer gsTimer;
+
+/* Inicialización del temporizador */
 void Timer_Inicializar(void)
 {
-
   TIM_MasterConfigTypeDef sMasterConfig;
-
-  Timer.Instance = TIM7;
-  Timer.Init.Prescaler = 108;
-  Timer.Init.CounterMode = TIM_COUNTERMODE_DOWN;
-  Timer.Init.Period = 999;
-  HAL_TIM_Base_Init(&Timer);
+	gsTimer.Timer.Instance = TIM7;
+  gsTimer.Timer.Init.Prescaler = 108;
+  gsTimer.Timer.Init.CounterMode = TIM_COUNTERMODE_DOWN;
+  gsTimer.Timer.Init.Period = 999;
+  HAL_TIM_Base_Init(&gsTimer.Timer);
 
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&Timer, &sMasterConfig);
-	HAL_NVIC_SetPriority(TIM7_IRQn,2,0);
+  HAL_TIMEx_MasterConfigSynchronization(&gsTimer.Timer, &sMasterConfig);
+	HAL_NVIC_SetPriority(TIM7_IRQn,5,0);
 	HAL_NVIC_EnableIRQ(TIM7_IRQn);
+	
+	HAL_TIM_Base_Start_IT(&gsTimer.Timer);
+}
+
+
+/* Handler de interrupción temporizador */
+void TIM7_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&gsTimer.Timer);
+	
+	if(gsTimer.BaseTime.MilisCounter[ec_Buzzer] == 0) return;
+	if(gsTimer.BaseTime.MilisCounter[ec_Delay] == 0) return;
+	
+	gsTimer.BaseTime.MilisCounter[ec_Buzzer]--;
+	gsTimer.BaseTime.MilisCounter[ec_Delay]--;
 }
 
 #endif
